b3ad1c4108dee7d73b98e6de6e12a280788997bd
radik-ha <rathikaari1@gmail.com>
2024-06-11 20:25:10 +0530

    Add files via upload

---
+++ drawHeart.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.bgcolor("black")
+turtle.title("Heart")
+pen = turtle.Pen()
+
+pen.pensize(3)
+pen.begin_fill()
+pen.fillcolor("red")
+pen.lt(140)
+pen.fd(170)
+pen.circle(-70,180)
+pen.setheading(60)
+pen.circle(-70,180)
+pen.fd(170)
+pen.end_fill()
---
+++ drawTriangle.py
@@ -0,0 +1,12 @@
+import turtle
+pen = turtle.Pen()
+pen.pensize(3)
+pen.color("white")
+pen.begin_fill()
+pen.fillcolor("blue")
+pen.fd(210)
+pen.lt(120)
+pen.fd(210)
+pen.lt(120)
+pen.fd(210)
+pen.end_fill()
